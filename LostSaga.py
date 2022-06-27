1e389ad3473212ecc3b741667727d2bbe8e1f366
IlhamApriansyah <ilhamapriansyah471@gmail.com>
2022-06-27 10:47:51 +0700

    Fix 27-06-2022

---
+++ LostSaga.py
@@ -0,0 +1,16 @@
+from operator import index
+import random as rd
+
+def Normal():
+    list = ["Iron knight", "Berserker", "Robin hood", "Space trooper"]
+
+    print(list[-2])
+
+def Premium():
+    list = {"BlazeBlue" : "Jin Kisaragi" , "GOD" : "Jin mori"}
+
+    print(list)
+
+
+Normal()
+Premium()
